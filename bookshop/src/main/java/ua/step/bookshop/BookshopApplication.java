8b13d3f00b3ff230c699056a2d22f6b85139477c
Vitaliy <vitalikp780@gmail.com>
2019-05-14 20:58:46 +0300

    first brunch commit

--- bookshop/src/main/java/ua/step/bookshop/BookshopApplication.java
+++ bookshop/src/main/java/ua/step/bookshop/BookshopApplication.java
@@ -3,11 +3,11 @@ package ua.step.bookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BookshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookshopApplication.class, args);
 	}
-
 }
