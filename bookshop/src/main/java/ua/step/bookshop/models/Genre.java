b518b659bfebd545a081c594157c6a2b6d33c954
Pobe_ebl5 <Pobe_ebl5@210-7P.od.itstep.local>
2019-05-14 20:49:19 +0300

    adding @Date to Genre.java and Publisher.java

--- bookshop/src/main/java/ua/step/bookshop/models/Genre.java
+++ bookshop/src/main/java/ua/step/bookshop/models/Genre.java
@@ -6,23 +6,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "Genres")
+@Data
 public class Genre {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Short id;
 	private String name;
-	public Short getId() {
-		return id;
-	}
-	public void setId(Short id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 }
--- bookshop/src/main/java/ua/step/bookshop/models/Publisher.java
+++ bookshop/src/main/java/ua/step/bookshop/models/Publisher.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 /**
  * 
  * @author Константин
@@ -11,22 +13,9 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "Publishers")
+@Data
 public class Publisher {
 	@Id
 	private Short id;
 	private String name;
-	
-	public Short getId() {
-		return id;
-	}
-	public void setId(Short id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
