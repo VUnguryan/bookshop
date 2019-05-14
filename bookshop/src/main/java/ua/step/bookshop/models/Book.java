b0a52f14f21b090337fbd2af1c9b2c165a8c7574
Константин <@DESKTOP-V8E4228>
2019-05-14 23:52:28 +0300

    debag error

--- bookshop/src/main/java/ua/step/bookshop/models/Book.java
+++ bookshop/src/main/java/ua/step/bookshop/models/Book.java
@@ -14,13 +14,80 @@ public class Book {
 	@Id
 	private Integer id;
 	private String name;
-	private Integer authorId;
+	private Integer authorId; // потом заменю на List<author> authors
 	private Integer year;
-	private Short genreId;
-	private Integer publisherId; 	
+	private Short genreId; // потом заменю на List<Genre>
+	private Integer publisherId; 	// заменить на Publisher publisher
 	private Integer price;
 	private Double rate;
-	private Integer adminId;
-	private Date createIate;
+	private Integer adminId; // заменить на Admin admin
+	private Date createDate;
 	private String background;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(Integer authorId) {
+		this.authorId = authorId;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public Short getGenreId() {
+		return genreId;
+	}
+	public void setGenreId(Short genreId) {
+		this.genreId = genreId;
+	}
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Double getRate() {
+		return rate;
+	}
+	public void setRate(Double rate) {
+		this.rate = rate;
+	}
+	public Integer getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+	public Date getCreateIate() {
+		return getCreateIate();
+	}
+	public void setCreateIate(Date createIate) {
+		this.createDate = createIate;
+	}
+	public String getBackground() {
+		return background;
+	}
+	public void setBackground(String background) {
+		this.background = background;
+	}
 }
--- bookshop/src/main/java/ua/step/bookshop/models/Publisher.java
+++ bookshop/src/main/java/ua/step/bookshop/models/Publisher.java
@@ -18,4 +18,16 @@ public class Publisher {
 	@Id
 	private Short id;
 	private String name;
+	public Short getId() {
+		return id;
+	}
+	public void setId(Short id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
