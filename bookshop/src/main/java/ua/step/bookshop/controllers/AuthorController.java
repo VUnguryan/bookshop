4432b267ee0c8a8f7fbd87b12ee51f7fe7b9f2b7
sergey <sergey@sergey-ThinkPad-Edge-E531>
2019-05-15 22:31:01 +0300

    Added author page and AddAuthor page. Developed controller, model and repository for author.

---
+++ bookshop/src/main/java/ua/step/bookshop/controllers/AuthorController.java
@@ -0,0 +1,35 @@
+package ua.step.bookshop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ua.step.bookshop.models.Author;
+import ua.step.bookshop.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@GetMapping("/authors")
+	private String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors";
+	}
+
+	@GetMapping("/authors/addAuthor")
+	private String getAddAuthor(@ModelAttribute Author author, Model model) {
+		return "addAuthor";
+	}
+
+	@PostMapping("/authors/addAuthor")
+	private String addAuthor(@ModelAttribute Author author) {
+		authorRepository.saveAndFlush(author);
+		return "redirect:/authors";
+	}
+}
---
+++ bookshop/src/main/java/ua/step/bookshop/models/Author.java
@@ -0,0 +1,24 @@
+package ua.step.bookshop.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Authors")
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+	private String name;
+	private String biography;
+}
