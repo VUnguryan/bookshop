b70498da016874e019a08446ee8f312dd29056ad
AnchikM <AnchikM@DESKTOP-OJB9H2G.Dlink>
2019-05-15 21:35:42 +0300

    Главная страница с меню демо

---
+++ bookshop/src/main/java/ua/step/bookshop/controllers/MainController.java
@@ -0,0 +1,23 @@
+package ua.step.bookshop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ua.step.bookshop.repositories.BookRepository;
+import ua.step.bookshop.repositories.GenreRepository;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	private BookRepository repo;
+
+	@GetMapping("/index")
+	private String getIndex(Model model) {
+		model.addAttribute("books", repo.findAll());
+		return "index";
+	}
+	
+}
