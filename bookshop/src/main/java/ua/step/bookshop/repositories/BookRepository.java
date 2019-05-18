8550c8e4fb4f7982f91594d6e09e56864d16050b
DimasikAlmazzik <chda98@bk.ru>
2019-05-18 22:23:00 +0300

    Добавил сортировку книг по дате добавления

--- bookshop/src/main/java/ua/step/bookshop/repositories/BookRepository.java
+++ bookshop/src/main/java/ua/step/bookshop/repositories/BookRepository.java
@@ -1,10 +1,12 @@
 package ua.step.bookshop.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.step.bookshop.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 	
-	
+	List<Book> findByOrderByCreateDate();
 	
 }
