5fbeaea899aa7bfb4d528b05064724c841a6dff8
voin947 <andrewpoberezhnik@gmail.com>
2019-05-19 22:21:12 +0300

    добавление пажинации в index.html

--- bookshop/src/main/java/ua/step/bookshop/controllers/MainController.java
+++ bookshop/src/main/java/ua/step/bookshop/controllers/MainController.java
@@ -1,15 +1,14 @@
 package ua.step.bookshop.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import ua.step.bookshop.models.Author;
+import org.springframework.web.bind.annotation.PathVariable;
+import ua.step.bookshop.models.Book;
 import ua.step.bookshop.repositories.AuthorRepository;
 import ua.step.bookshop.repositories.BookRepository;
 import ua.step.bookshop.repositories.GenreRepository;
@@ -26,13 +25,36 @@ public class MainController {
 	@Autowired
 	private AuthorRepository repoA;
 
+	private static int BOOKSONPAGE = 9;
+
 	@GetMapping("/")
 	private String getIndex(Model model) {
-		model.addAttribute("books", repo.findAll());
+		return getBooksInPage(model, 1);
+	}
+
+	@GetMapping("/lib/{page}")
+	private String getPaginatedIndex(Model model, @PathVariable int page) {
+		return getBooksInPage(model, page);
+	}
+
+	String getBooksInPage(Model model, int page) {
+		List<Book> allBooks = repo.findAll();
+		List<Book> books = new ArrayList<>();
+
+		int pages = (int) Math.ceil((double) allBooks.size() / BOOKSONPAGE);
+
+		for(int i = (page-1) * BOOKSONPAGE; i < (page) * BOOKSONPAGE && i < allBooks.size(); i ++) {
+			books.add(allBooks.get(i));
+		}
+
+		model.addAttribute("curpage", page);
+		model.addAttribute("pages", pages);
+		model.addAttribute("books", books);
 		model.addAttribute("genres", repoJ.findAll());
 		model.addAttribute("publishers", repoP.findAll());
 		model.addAttribute("authors", repoA.findAll());
 		model.addAttribute("contentPage", "books");
+
 		return "index";
 	}
 
