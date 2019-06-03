4d4fb10919be4b4af29d93022fc11a5bf675638f
VUnguryan <VUnguryan@VUnguryan.kiev.luxoft.com>
2019-06-03 09:48:15 +0300

    добавление дериктории для тестов и пример теста репозитария и модели.

---
+++ bookshop/src/test/java/ua/step/bookshop/AuthorRepositoryTest.java
@@ -0,0 +1,38 @@
+package ua.step.bookshop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ua.step.bookshop.models.Author;
+import ua.step.bookshop.repositories.AuthorRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AuthorRepositoryTest {
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private AuthorRepository repository;
+	
+	@Test
+	public void testAuthorSave() {
+		Author author = new Author();
+		author.setId(1);
+		author.setName("abc");
+		author.setBiography("qwerty");
+		// сохраняем
+		this.entityManager.persist(author);
+		// получаем
+		author = this.repository.getOne(1);
+		// проверяем
+		assertThat(author.getName()).isEqualTo("abc");
+		assertThat(author.getBiography()).isEqualTo("qwerty");
+	}
+}
