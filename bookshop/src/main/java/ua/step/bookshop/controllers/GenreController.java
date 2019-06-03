ef7446da89f24e65d90e0318eeb683fbc6529b40
Vitaliy <vitalikp780@gmail.com>
2019-06-03 23:27:15 +0300

    Add sort by genre

--- bookshop/src/main/java/ua/step/bookshop/controllers/GenreController.java
+++ bookshop/src/main/java/ua/step/bookshop/controllers/GenreController.java
@@ -9,23 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import ua.step.bookshop.models.Book;
 import ua.step.bookshop.models.Genre;
 import ua.step.bookshop.repositories.BookRepository;
-import ua.step.bookshop.repositories.GenreRepository;
 
 @Controller
 public class GenreController {
 	@Autowired
-	private GenreRepository repoG;
 	private BookRepository repoB;
-
-/*	@GetMapping("/genres")
-	public String getGenres(Model model) {
-		model.addAttribute("genres", repoG.findAll());
-		return "genres";
-	}*/
 
 	@GetMapping("/sortByGenre/{id}")
 	private String getGenres(@PathVariable("id") Short id, Model model) {
@@ -37,14 +28,14 @@ public class GenreController {
 			genresList = book.getGenreList();
 			for (Genre genre : genresList) {
 				Short sortByGenreName = genre.getId();
-				if (sortByGenreName == id) {
+				if (sortByGenreName.equals(id)) {
 					sortsdByGenres.add(book);
 				}
 			}
 		}
 		model.addAttribute("books", sortsdByGenres);
-		model.addAttribute("contentPage", "genres");
-		return "index";
-		//return "redirect:/books/" + id;
+		model.addAttribute("contentPage", "/fragments/books");
+		return "sortByGenre";
+		// return "index";
 	}
 }
