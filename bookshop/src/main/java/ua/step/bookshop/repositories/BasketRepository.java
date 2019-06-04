894e2d6ad13bf4b10b840d986dc2213e9549d22a
sergey <sergey@sergey-ThinkPad-Edge-E531>
2019-06-04 03:18:58 +0300

    Удаление лишнего

--- bookshop/src/main/java/ua/step/bookshop/repositories/BasketRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.step.bookshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ua.step.bookshop.models.Basket;
-
-public interface BasketRepository extends JpaRepository<Basket, Integer> {
-}
