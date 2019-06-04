70788ff3ebb440bcdcf59aac1bc0ad3d896a6b78
Константин <@10.4.0.128>
2019-06-04 20:07:27 +0300

    сортировка по жанру

--- bookshop/src/main/java/ua/step/bookshop/controllers/GenreController.java
+++ bookshop/src/main/java/ua/step/bookshop/controllers/GenreController.java
@@ -1,20 +1,54 @@
 package ua.step.bookshop.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ua.step.bookshop.repositories.GenreRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import ua.step.bookshop.models.Book;
+import ua.step.bookshop.models.Genre;
+import ua.step.bookshop.repositories.BookRepository;
+
+/**
+ * 
+ * @author Vitaly Sakaly
+ *
+ */
 
 @Controller
 public class GenreController {
-	@Autowired
-	private GenreRepository repo;
+  @Autowired
+  private BookRepository repoB;
 
-	@GetMapping("/genres")
-	public String getGenres(Model model) {
-		model.addAttribute("genres", repo.findAll());
-		model.addAttribute("contentPage", "genres");
-		return "index";
-	}
+  @GetMapping("/sortByGenre/{id}")
+  private String getGenres(@PathVariable("id") Short id, Model model) {
+    List<Book> allBooks = repoB.findAll();
+    List<Book> sortsdByGenres = new ArrayList<Book>();
+    List<Genre> genresList;
+
+    for (Book book : allBooks) {
+      genresList = book.getGenres();
+      for (Genre genre : genresList) {
+        Short sortByGenreName = genre.getId();
+        if (sortByGenreName.equals(id)) {
+          sortsdByGenres.add(book);
+        }
+      }
+    }
+    model.addAttribute("books", sortsdByGenres);
+    model.addAttribute("contentPage", "/fragments/books");
+    return "sortByGenre";
+    // return "index";
+
+  /*@GetMapping("/genres")
+  public String getGenres(Model model) {
+    model.addAttribute("genres", repo.findAll());
+    model.addAttribute("contentPage", "genres");
+    return "index";*/
+
+  }
 }
